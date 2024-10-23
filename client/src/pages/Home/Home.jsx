12f9e34efa9664c67d207e9f3d87745a4db33c1a
SanadAlawi <sanadalawi20@gmail.com>
2024-10-23 19:10:04 +0200

    Fix images

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,24 +2,26 @@ import React from 'react'
 import style from './style.module.css'
 
 const Home = () => {
+
+
   return (
     <section className={style['home']}>
 
 
       <div className={style['grid']}>
         <div className={style['box']}>
-          <img className={style['image']} src='https://source.unsplash.com//a-coat-hanging-on-a-clothes-line-M5F4vmEnxsA' alt="" />
+          <img className={style['image']} src='/images/white dress 1.jpeg' alt="" />
           <span className={style['label']}>Women</span>
         </div>
 
         <div className={style['box']}>
-          <img className={style['image']} src='https://source.unsplash.com/brown-long-sleeve-shirt-on-white-clothes-hanger-Fg15LdqpWrs' alt="" />
+          <img className={style['image']} src='/images/green jacket 2.jpeg' alt="" />
           <span className={style['label']}>Men</span>
         </div>
       </div>
 
-      <img className={style['image']} src='https://source.unsplash.com//a-coat-hanging-on-a-clothes-line-M5F4vmEnxsA' alt="" />
-      <img className={style['image']} src='https://source.unsplash.com/brown-long-sleeve-shirt-on-white-clothes-hanger-Fg15LdqpWrs' alt="" />
+      <img className={style['image']} src='/images/red jacket 1.jpeg' alt="" />
+      <img className={style['image']} src='/images/white dress 6.jpeg' alt="" />
 
     </section>
   )
